4b655e227480f73645fd759d118450789a84a253
Sébastien Combéfis <seb478@gmail.com>
2015-07-13 07:01:43 +0200

    Add methods to Matrix class

--- be/ukonline/mathlib/Matrix.java
+++ be/ukonline/mathlib/Matrix.java
@@ -22,7 +22,7 @@ public final class Matrix
 	 */
 	public Matrix (int m, int n)
 	{
-		data = null;
+		data = new double[m][n];
 	}
 	
 	/**
@@ -58,5 +58,34 @@ public final class Matrix
 	public double get (int i, int j)
 	{
 		return 0;
+	}
+	
+	/**
+	 * Tests whether the matrix is squared
+	 * 
+	 * @pre -
+	 * @post The returned value contains true is this matrix is squared,
+	 *       i.e. has the same number of rows and columns,
+	 *       and false otherwise
+	 */
+	public boolean isSquareMatrix()
+	{
+		return false;
+	}
+	
+	/**
+	 * Gets a string representation of the matrix
+	 * 
+	 * @pre -
+	 * @post The returned value contains a string representation of this matrix
+	 *       Each element is represented with its own toString() method.
+	 *       For example, the identity 2x2 matrix is:
+	 *        [1.0, 0.0]
+	 *        [0.0, 1.0] 
+	 */
+	@Override
+	public String toString()
+	{
+		return null;
 	}
 }
--- be/ukonline/mathlib/test/TestMatrix.java
+++ be/ukonline/mathlib/test/TestMatrix.java
@@ -13,43 +13,95 @@ import be.ukonline.mathlib.Matrix;
  * Test class of the Matrix class
  * 
  * @author Sébastien Combéfis
- * @version June 21, 2015
+ * @version July 13, 2015
  */
 public final class TestMatrix
 {
 	// Instance variables
-	private Matrix m;
+	private Matrix matrixA, matrixB;
 	
 	@Before
 	public void setUp() throws Exception
 	{
-		m = new Matrix (4, 7);
+		matrixA = new Matrix (3, 7);
+		matrixB = new Matrix (5, 2);
 	}
 
 	@Test
 	public void testNbRows()
 	{
-		// The matrix m must have 4 rows
-		assertEquals (4, m.nbRows());
+		// "matrixA" must have 3 rows
+		assertEquals (3, matrixA.nbRows());
+		
+		// "matrixB" must have 5 rows
+		assertEquals (5, matrixB.nbRows());
 	}
 
 	@Test
 	public void testNbColumns()
 	{
-		// The matrix m must have 7 columns
-		assertEquals (7, m.nbColumns());
+		// "matrixA" must have 7 columns
+		assertEquals (7, matrixA.nbColumns());
+		
+		// "matrixB" must have 2 columns
+		assertEquals (2, matrixB.nbColumns());
 	}
 
 	@Test
 	public void testGet()
 	{
-		// All the values of the matrix m must be 0
-		for (int i = 1; i <= m.nbRows(); i++)
+		// All the values of "matrixA" must be 0
+		for (int i = 1; i <= matrixA.nbRows(); i++)
 		{
-			for (int j = 1; j <= m.nbColumns(); j++)
+			for (int j = 1; j <= matrixA.nbColumns(); j++)
 			{
-				assertEquals (0, m.get(i, j), 0);
+				assertEquals (0, matrixA.get(i, j), 0);
 			}
 		}
+		
+		// All the values of "matrixB" must be 0
+		for (int i = 1; i <= matrixB.nbRows(); i++)
+		{
+			for (int j = 1; j <= matrixB.nbColumns(); j++)
+			{
+				assertEquals (0, matrixB.get(i, j), 0);
+			}
+		}
+	}
+	
+	@Test
+	public void testIsSquareMatrix()
+	{
+		// "matrixA" and "matrixB" are not squared matrices
+		assertFalse (matrixA.isSquareMatrix());
+		assertFalse (matrixB.isSquareMatrix());
+	}
+	
+	@Test
+	public void testToString()
+	{
+		String expected;
+		
+		// String representation of "matrixA" must be
+		// [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+		// [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+		// [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+		expected = "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]\n"
+				 + "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]\n"
+				 + "[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]";
+		assertEquals (expected, matrixA.toString());
+		
+		// String representation of "matrixB" must be
+		// [0.0, 0.0]
+		// [0.0, 0.0]
+		// [0.0, 0.0]
+		// [0.0, 0.0]
+		// [0.0, 0.0]
+		expected = "[0.0, 0.0]\n"
+				 + "[0.0, 0.0]\n"
+				 + "[0.0, 0.0]\n"
+				 + "[0.0, 0.0]\n"
+				 + "[0.0, 0.0]";
+		assertEquals (expected, matrixB.toString());
 	}
 }
