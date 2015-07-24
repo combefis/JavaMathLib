871594471fa722eeb0f2fb246e3606107d2c23b1
Sébastien Combéfis <seb478@gmail.com>
2015-07-24 16:50:19 +0200

    Add classes to represent functions
    - Added a Function abstract class to represent a generic function
    - Added one concrete Sinus class
    - Added test class for Sinus

---
+++ be/ukonline/mathlib/Function.java
@@ -0,0 +1,21 @@
+// Function.java
+
+package be.ukonline.mathlib;
+
+/**
+ * Function
+ * 
+ * @author Sébastien Combéfis
+ * @version July 24, 2015
+ */
+public abstract class Function
+{
+	/**
+	 * Gets the value of the function
+	 * 
+	 * @pre -
+	 * @post The returned value contains the value of the function
+	 *       for the specified "x" value
+	 */
+	public abstract double getValue (double x);
+}
---
+++ be/ukonline/mathlib/Sinus.java
@@ -0,0 +1,18 @@
+// Sinus.java
+
+package be.ukonline.mathlib;
+
+/**
+ * Sinus function
+ * 
+ * @author Sébastien Combéfis
+ * @version July 24, 2015
+ */
+public class Sinus extends Function
+{
+	@Override
+	public double getValue (double x)
+	{
+		return 0;
+	}
+}
